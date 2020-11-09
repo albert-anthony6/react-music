c29f7253f39f7990f4246e6fba0c2bdfff7a742c
Albert Valdes <albert.valdes@24g.com>
2020-11-09 16:21:56 -0600

    First Commit

---
+++ src/components/Song.js
@@ -0,0 +1,14 @@
+// This component shows the song's name, artist, and picture
+import React from 'react';
+
+const Song = () => {
+    return (
+        <div className="song-container">
+            <h1>Picture</h1>
+            <h1>Song Name</h1>
+            <h1>Artist</h1>
+        </div>
+    );
+};
+
+export default Song;
